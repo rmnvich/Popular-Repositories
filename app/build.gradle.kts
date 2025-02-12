bfcf9ec646250d6b5dcd7b26c057912dd349db3d
vromanovich <romanovich.personal@gmail.com>
2025-02-12 15:53:06 +0100

    Used extensions for added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,29 +1,21 @@
 plugins {
+    alias(libs.plugins.ksp)
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt)
 }
 
 android {
     namespace = "com.splunk.test"
+
     buildFeatures {
         compose = true
     }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(platform(libs.compose.bom))
-    implementation(libs.compose.activity)
-    implementation(libs.compose.ui)
-    implementation(libs.compose.ui.graphics)
-    implementation(libs.compose.ui.tooling.preview)
-    implementation(libs.compose.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.junit)
-    androidTestImplementation(libs.espresso.core)
-    androidTestImplementation(platform(libs.compose.bom))
-    debugImplementation(libs.compose.ui.tooling)
+    androidX()
+    compose()
+    hilt()
 }
