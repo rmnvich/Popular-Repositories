5ca15ef3ccb03523599a56169db167af6243d111
vromanovich <romanovich.personal@gmail.com>
2025-02-12 15:19:04 +0100

    Cleaned up :app: gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,31 +6,6 @@ plugins {
 
 android {
     namespace = "com.splunk.test"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.splunk.test"
-        minSdk = 26
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
     buildFeatures {
         compose = true
     }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,42 @@
+import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompilerOptions
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
+}
+
+subprojects {
+    plugins.withId("com.android.application") {
+        configure<BaseAppModuleExtension> {
+            compileSdk = 34
+            defaultConfig {
+                versionCode = 1
+                versionName = "1.0.0"
+            }
+            defaultConfig {
+                minSdk = 26
+                targetSdk = 34
+
+                testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                vectorDrawables {
+                    useSupportLibrary = true
+                }
+            }
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_1_8
+                targetCompatibility = JavaVersion.VERSION_1_8
+            }
+        }
+
+        tasks.withType<KotlinCompilationTask<*>>().configureEach {
+            val options = compilerOptions as KotlinJvmCompilerOptions
+            options.jvmTarget.set(JvmTarget.JVM_1_8)
+        }
+    }
 }
