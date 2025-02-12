2efed6c288760067eb25e5ddec05cba4c0d8939e
vromanovich <romanovich.personal@gmail.com>
2025-02-12 18:02:34 +0100

    Created DI NetworkModule

---
+++ data/src/main/java/com/splunk/test/data/di/NetworkModule.kt
@@ -0,0 +1,50 @@
+package com.splunk.test.data.di
+
+import com.splunk.test.data.utils.NetworkConstants
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .connectTimeout(NetworkConstants.TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .readTimeout(NetworkConstants.TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        moshi: Moshi,
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(NetworkConstants.BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .build()
+}
---
+++ data/src/main/java/com/splunk/test/data/utils/NetworkConstants.kt
@@ -0,0 +1,7 @@
+package com.splunk.test.data.utils
+
+object NetworkConstants {
+    const val BASE_URL = "https://api.github.com/"
+
+    const val TIMEOUT_SECONDS = 30L
+}
