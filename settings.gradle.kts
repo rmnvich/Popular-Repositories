75eeb63b8c057296a2702676ec8b36e9ee9031e8
vromanovich <romanovich.personal@gmail.com>
2025-02-12 14:54:00 +0100

    Created buildSrc module to use Kotlin DSL

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+rootProject.name = "SplunkTest"
+
 pluginManagement {
     repositories {
         google {
@@ -11,6 +13,7 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -19,6 +22,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "SplunkTest"
-include(":app")
- 
+include(
+    ":app",
+)
