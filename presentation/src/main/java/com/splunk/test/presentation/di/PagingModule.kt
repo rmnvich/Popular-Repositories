3317748cad7a39b891fda9675445bf5c52c3449f
vromanovich <romanovich.personal@gmail.com>
2025-02-12 21:32:31 +0100

    Update PagingModule

--- presentation/src/main/java/com/splunk/test/presentation/di/PagingModule.kt
+++ presentation/src/main/java/com/splunk/test/presentation/di/PagingModule.kt
@@ -1,19 +1,30 @@
 package com.splunk.test.presentation.di
 
+import androidx.paging.PagingConfig
 import androidx.paging.PagingSource
 import com.splunk.test.domain.model.GitHubRepository
+import com.splunk.test.domain.usecase.get.GetTrendingRepositoriesUseCase
+import com.splunk.test.presentation.paging.PagingConstants
 import com.splunk.test.presentation.paging.TrendingRepositoriesPagingSource
-import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 
 @Module
 @InstallIn(ViewModelComponent::class)
-interface PagingModule {
+object PagingModule {
 
-    @Binds
-    fun bindTrendingRepositoriesPagingSource(
-        impl: TrendingRepositoriesPagingSource,
-    ): PagingSource<Int, GitHubRepository>
+    @Provides
+    fun provideTrendingRepositoriesPagingSource(
+        getTrendingRepositoriesUseCase: GetTrendingRepositoriesUseCase,
+    ): PagingSource<Int, GitHubRepository> = TrendingRepositoriesPagingSource(
+        getTrendingRepositoriesUseCase = getTrendingRepositoriesUseCase,
+    )
+
+    @Provides
+    fun providePagingConfig(): PagingConfig = PagingConfig(
+        pageSize = PagingConstants.TRENDING_REPOSITORIES_PAGE_SIZE,
+        enablePlaceholders = PagingConstants.TRENDING_REPOSITORIES_ENABLE_PLACEHOLDERS,
+    )
 }
