ec909ea9c9728d98caf755d2e9fc71b1fd269ad7
vromanovich <romanovich.personal@gmail.com>
2025-02-12 19:11:47 +0100

    Added comment

--- data/src/main/java/com/splunk/test/data/utils/NetworkConstants.kt
+++ data/src/main/java/com/splunk/test/data/utils/NetworkConstants.kt
@@ -1,6 +1,8 @@
 package com.splunk.test.data.utils
 
 object NetworkConstants {
+    //WARNING: Better to store in NDK, or at least in remote config.
+    //I'll leave this here to keep things simple, considering this is a public API.
     const val BASE_URL = "https://api.github.com/"
 
     const val TIMEOUT_SECONDS = 30L
