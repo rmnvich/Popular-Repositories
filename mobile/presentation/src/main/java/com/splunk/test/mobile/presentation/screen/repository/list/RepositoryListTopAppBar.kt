2a0b59b8cf4f3c87412580040ebc80315046d8b6
vromanovich <romanovich.personal@gmail.com>
2025-02-13 16:47:08 +0100

    Updated TopAppBar title

--- mobile/presentation/src/main/java/com/splunk/test/mobile/presentation/screen/repository/list/RepositoryListTopAppBar.kt
+++ mobile/presentation/src/main/java/com/splunk/test/mobile/presentation/screen/repository/list/RepositoryListTopAppBar.kt
@@ -22,6 +22,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -63,7 +64,10 @@ fun RepositoryListTopAppBar(
             expandedHeight = 56.dp,
             title = {
                 Text(
+                    modifier = Modifier.padding(horizontal = 8.dp),
                     text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.SemiBold,
                     color = MaterialTheme.colorScheme.onPrimary,
                 )
             },
