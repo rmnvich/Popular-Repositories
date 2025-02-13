6fe1fa095cc5b11ddc747f96966bb917f12f943c
vromanovich <romanovich.personal@gmail.com>
2025-02-13 17:35:18 +0100

    Updated shimmer color

--- mobile/presentation/src/main/java/com/splunk/test/mobile/presentation/utils/widget/ShimmerBrush.kt
+++ mobile/presentation/src/main/java/com/splunk/test/mobile/presentation/utils/widget/ShimmerBrush.kt
@@ -5,20 +5,21 @@ import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 
 private const val LABEL_SHIMMER_TRANSITION = "shimmer_transition"
 private const val LABEL_SHIMMER_TRANSLATE_ANIMATION = "shimmer_translate_animation"
 
 @Composable
-fun shimmerBrush(width: Float = 1000f): Brush {
+fun shimmerBrush(width: Float = 1300f): Brush {
+    val baseColor = MaterialTheme.colorScheme.secondaryContainer
     val shimmerColors = listOf(
-        Color.LightGray.copy(alpha = 0.6f),
-        Color.LightGray.copy(alpha = 0.2f),
-        Color.LightGray.copy(alpha = 0.6f),
+        baseColor.copy(alpha = 0.6f),
+        baseColor.copy(alpha = 0.2f),
+        baseColor.copy(alpha = 0.6f),
     )
 
     val transition = rememberInfiniteTransition(label = LABEL_SHIMMER_TRANSITION)
@@ -26,7 +27,7 @@ fun shimmerBrush(width: Float = 1000f): Brush {
         initialValue = 0f,
         targetValue = width,
         animationSpec = infiniteRepeatable(
-            animation = tween(durationMillis = 1500),
+            animation = tween(durationMillis = 700),
             repeatMode = RepeatMode.Reverse,
         ),
         label = LABEL_SHIMMER_TRANSLATE_ANIMATION,
