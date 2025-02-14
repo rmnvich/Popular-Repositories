268705aa19265b7478c9af1c42f45f875aeacd02
vromanovich <romanovich.personal@gmail.com>
2025-02-14 14:52:53 +0100

    Reverted simple theme switching animation

--- mobile/theme/src/main/java/com/splunk/test/mobile/theme/Theme.kt
+++ mobile/theme/src/main/java/com/splunk/test/mobile/theme/Theme.kt
@@ -2,14 +2,22 @@ package com.splunk.test.mobile.theme
 
 import android.app.Activity
 import android.os.Build
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+
+private const val LABEL_SWITCH_THEME_ANIMATION = "animation_switch_theme"
+private const val SWITCH_THEME_ANIMATION_DURATION = 300
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -44,8 +52,58 @@ fun SplunkTestTheme(
 
     val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = colorScheme.switch(),
         typography = Typography,
         content = content
     )
+}
+
+@Composable
+private fun ColorScheme.switch() = copy(
+    primary = animateColor(primary),
+    onPrimary = animateColor(onPrimary),
+    primaryContainer = animateColor(primaryContainer),
+    onPrimaryContainer = animateColor(onPrimaryContainer),
+    inversePrimary = animateColor(inversePrimary),
+    secondary = animateColor(secondary),
+    onSecondary = animateColor(onSecondary),
+    secondaryContainer = animateColor(secondaryContainer),
+    onSecondaryContainer = animateColor(onSecondaryContainer),
+    tertiary = animateColor(tertiary),
+    onTertiary = animateColor(onTertiary),
+    tertiaryContainer = animateColor(tertiaryContainer),
+    onTertiaryContainer = animateColor(onTertiaryContainer),
+    background = animateColor(background),
+    onBackground = animateColor(onBackground),
+    surface = animateColor(surface),
+    onSurface = animateColor(onSurface),
+    surfaceVariant = animateColor(surfaceVariant),
+    onSurfaceVariant = animateColor(onSurfaceVariant),
+    surfaceTint = animateColor(surfaceTint),
+    inverseSurface = animateColor(inverseSurface),
+    inverseOnSurface = animateColor(inverseOnSurface),
+    error = animateColor(error),
+    onError = animateColor(onError),
+    errorContainer = animateColor(errorContainer),
+    onErrorContainer = animateColor(onErrorContainer),
+    outline = animateColor(outline),
+    outlineVariant = animateColor(outlineVariant),
+    scrim = animateColor(scrim),
+    surfaceBright = animateColor(surfaceBright),
+    surfaceDim = animateColor(surfaceDim),
+    surfaceContainer = animateColor(surfaceContainer),
+    surfaceContainerHigh = animateColor(surfaceContainerHigh),
+    surfaceContainerHighest = animateColor(surfaceContainerHighest),
+    surfaceContainerLow = animateColor(surfaceContainerLow),
+    surfaceContainerLowest = animateColor(surfaceContainerLowest)
+)
+
+@Composable
+private fun animateColor(targetColor: Color): Color {
+    val animatedColor by animateColorAsState(
+        targetValue = targetColor,
+        animationSpec = tween(durationMillis = SWITCH_THEME_ANIMATION_DURATION),
+        label = LABEL_SWITCH_THEME_ANIMATION
+    )
+    return animatedColor
 }
