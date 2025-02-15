08121f79c6346d5333473a13eb3bc7d6fc14d9b0
vromanovich <romanovich.personal@gmail.com>
2025-02-15 20:15:01 +0100

    Added annotations

--- mobile/presentation/src/main/java/com/splunk/test/mobile/presentation/MainActivity.kt
+++ mobile/presentation/src/main/java/com/splunk/test/mobile/presentation/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -15,6 +18,9 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    @ExperimentalLayoutApi
+    @ExperimentalMaterial3Api
+    @ExperimentalSharedTransitionApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
